6505cb0ae2d994d9f498d66a80fced7db6820281
Thinh <thinh1198@hotmail.com>
2024-05-01 23:58:15 +0100

    inital layout design

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,19 @@
-import { useState, useRef, useEffect } from 'react'
+import { useState, useRef, useEffect } from 'react';
+import Navbar from './components/navbar/navbar';
 
 
 function App() {
-  
+
+
+    return (
+        <div className='page'>
+            <header className='header header--container'>
+                <div className='header--inner'>
+                    test
+                </div>
+            </header>
+        </div>
+    )
 }
 
-export default App
+export default App;
---
+++ src/components/navbar/navbar.jsx
@@ -0,0 +1,12 @@
+
+function Navbar() {
+
+
+    return (
+        <nav>
+
+        </nav>
+    )
+}
+
+export default Navbar;
